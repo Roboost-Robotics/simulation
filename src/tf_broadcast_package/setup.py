79299c436deb655d50dcee3d09be38a571968beb
Geibinger <friedl.jak@gmail.com>
2023-10-29 21:07:54 +0100

    added relative paths, updated STL files

--- src/tf_broadcast_package/setup.py
+++ src/tf_broadcast_package/setup.py
@@ -10,11 +10,14 @@ setup(
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", "ament_index", "resource_index", "packages"),
+            [os.path.join("resource", package_name)],
+        ),
+        (os.path.join("share", package_name), ["package.xml"]),
         (os.path.join("share", package_name), glob("launch/*.py")),
         (os.path.join("share", package_name), glob("urdf/*")),
-        (os.path.join("share", package_name), glob("meshes/*")),
+        (os.path.join("share", package_name, "meshes"), glob("meshes/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
